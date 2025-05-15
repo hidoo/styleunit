1af84d553c2d20fa9c22bf5595e24154e8951c77
hidoo <4569054+hidoo@users.noreply.github.com>
2025-05-15 22:12:58 +0900

    chore(deps): update @hidoo/kss-builder to v1.1.0

--- task/styleguide.js
+++ task/styleguide.js
@@ -5,27 +5,32 @@ import buildStyleguide from '@hidoo/gulp-task-build-styleguide-kss';
 import * as config from '../config.js';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
+const options = {
+  builder: path.resolve(dirname, '../node_modules/@hidoo/kss-builder'),
+  'pseudo-class-transforms': false,
+  'section-duplicates': false
+};
 
 // define example tasks
 export const unit = buildStyleguide({
+  ...options,
   name: 'styleguide:main',
   src: `${config.path.srcStyleguide}/main.css`,
   dest: config.path.destStyleguide,
   get css() {
     return `${path.relative(this.dest, path.dirname(this.src))}/main.css`;
   },
-  homepage: path.resolve(dirname, '../README.md'),
-  builder: path.resolve(dirname, '../node_modules/@hidoo/kss-builder')
+  homepage: path.resolve(dirname, '../README.md')
 });
 export const pluginSpritesheet = buildStyleguide({
+  ...options,
   name: 'styleguide:plugin:spritesheet',
   src: `${config.path.srcStyleguide}/plugin/spritesheet/main.css`,
   dest: `${config.path.destStyleguide}/plugin/spritesheet`,
   get css() {
     return `${path.relative(this.dest, path.dirname(this.src))}/main.css`;
   },
-  homepage: path.resolve(dirname, '../src/plugin/spritesheet/README.md'),
-  builder: path.resolve(dirname, '../node_modules/@hidoo/kss-builder')
+  homepage: path.resolve(dirname, '../src/plugin/spritesheet/README.md')
 });
 
 // define main task
